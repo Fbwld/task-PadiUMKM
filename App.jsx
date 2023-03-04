8e8de7151c589e85a5b48db2d1f234eb699d36e0
fbwld <febiwldr09@gmail.com>
2023-03-05 01:33:30 +0700

    complete home

--- App.jsx
+++ App.jsx
@@ -1,18 +1,9 @@
 // In App.js in a new project
 
 import * as React from 'react';
-import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Router from './src/routers';
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text style={{color:'black'}}>Home Screen</Text>
-    </View>
-  );
-}
 
 const Stack = createNativeStackNavigator();
 
